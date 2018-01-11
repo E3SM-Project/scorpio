0a0dcdc80e9945b923e1124530e9870cb0fa9dc0
tkurc <tkurc>
2018-01-11 08:42:28 -0500

    Adding a library method for adios2pio-nm and removing global variables.

--- build/tests/performance/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/tkurc/codar/acme/ParallelIO/tests/performance
-# Build directory: /home/tkurc/codar/acme/ParallelIO/build/tests/performance
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
