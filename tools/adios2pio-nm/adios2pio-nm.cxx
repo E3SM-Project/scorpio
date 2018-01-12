0bfe3586ce22573a7175014e2eaa587b59885617
tkurc <tkurc>
2018-01-12 09:49:22 -0500

    Code cleanup.

--- tools/adios2pio-nm/adios2pio-nm.cxx
+++ tools/adios2pio-nm/adios2pio-nm.cxx
@@ -1,4 +1,7 @@
 #include <mpi.h>
+#ifdef TIMING
+#include <gptl.h>
+#endif
 #include "adios2pio-nm-lib.h"
 
 int main (int argc, char *argv[])
@@ -18,7 +21,20 @@ int main (int argc, char *argv[])
     string outfilename = argv[2];
 	string piotype     = argv[3];
 
+#ifdef TIMING
+    /* Initialize the GPTL timing library. */
+    if ((ret = GPTLinitialize ()))
+        return ret;
+#endif
+
+	SetDebugOutput(0);
 	ret = ConvertBPToNC(infilepath,outfilename,piotype);
+
+#ifdef TIMING
+    /* Finalize the GPTL timing library. */
+    if ((ret = GPTLfinalize ()))
+        return ret;
+#endif
 
     MPI_Finalize();
     return ret;
