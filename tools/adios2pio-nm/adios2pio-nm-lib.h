2c58e77c42ee5e46bc59b1ac1bcab6753c07daa8
tkurc <tkurc>
2018-01-12 09:49:22 -0500

    Code cleanup.

--- tools/adios2pio-nm/adios2pio-nm-lib.h
+++ tools/adios2pio-nm/adios2pio-nm-lib.h
@@ -7,5 +7,6 @@ using namespace std;
 
 int ConvertBPToNC(string infilepath, string outfilename, string piotype);
 void usage(string prgname, int mpirank);
+void SetDebugOutput(int val);
 
 #endif /* #ifndef _ADIOS2PIO_NM_LIB_H_ */
