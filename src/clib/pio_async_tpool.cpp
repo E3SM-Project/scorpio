36f5f9b7e0ae092bf3eae9f09fee548ad236b167
jayeshkrishna <jayesh@mcs.anl.gov>
2018-11-05 15:47:42 -0600

    Adding some debug log statements
    Adding some log statements to the thread and the pool classes.
    
    No change in code functionality

--- src/clib/pio_async_tpool.cpp
+++ src/clib/pio_async_tpool.cpp
@@ -15,6 +15,11 @@ static PIO_Util::PIO_async_tpool_manager tpool_mgr;
 void PIO_Util::PIO_async_tpool::enqueue(pio_async_op_t *op)
 {
   assert(op);
+  LOG((2, "PIO_async_tpool:enqueue: Enqueing async op, kind = %s",
+      (op->op_type == PIO_ASYNC_REARR_OP) ? "PIO_ASYNC_REARR_OP" :
+      ((op->op_type == PIO_ASYNC_PNETCDF_WRITE_OP) ? "PIO_ASYNC_PNETCDF_WRITE_OP" :
+      ((op->op_type == PIO_ASYNC_FILE_WRITE_OPS) ? "PIO_ASYNC_FILE_WRITE_OPS" :
+      "UNKNOWN"))));
   mtq_.enqueue(op);
 }
 
@@ -25,6 +30,7 @@ void PIO_Util::PIO_async_tpool::finalize(void )
 
 PIO_Util::PIO_async_tpool::PIO_async_tpool(int nthreads)
 {
+  LOG((2, "PIO_async_tpool:PIO_async_tpool: Creating %d threads", nthreads));
   for(int i=0; i<nthreads; i++){
     pool_threads_.push_back(std::thread(PIO_async_tpool::dequeue_and_process, this));
   }
@@ -32,6 +38,7 @@ PIO_Util::PIO_async_tpool::PIO_async_tpool(int nthreads)
 
 PIO_Util::PIO_async_tpool::~PIO_async_tpool()
 {
+  LOG((2, "PIO_async_tpool:~PIO_async_tpool: Sending STOP signal"));
   mtq_.signal(PIO_Util::PIO_mtq<pio_async_op_t *>::PIO_MTQ_SIG_STOP);
   for(std::vector<std::thread>::iterator iter = pool_threads_.begin();
       iter != pool_threads_.end(); ++iter){
@@ -50,6 +57,7 @@ int PIO_Util::PIO_async_tpool::dequeue_and_process(
    * pending asynchronous operations queued in the thread pool
    */
   do{
+    LOG((2, "PIO_async_tpool:dequeue_and_process: Waiting for async ops..."));
     pio_async_op_t *op = NULL;
     ret = tpool->mtq_.dequeue(op);
     if(ret == 0){
@@ -79,7 +87,11 @@ PIO_Util::PIO_async_tpool *
   /* We need to make NUM_THREADS configurable by the user (compile-time) */
   const int NUM_THREADS = 1;
   if(tpool_ == NULL){
+    LOG((2, "PIO_async_tpool_manager:get_tpool_instance: Creating new tpool instance"));
     tpool_ = new PIO_Util::PIO_async_tpool(NUM_THREADS);
+  }
+  else{
+    LOG((2, "PIO_async_tpool_manager:get_tpool_instance: Retrieving tpool instance"));
   }
   return tpool_;
 }
@@ -87,9 +99,13 @@ PIO_Util::PIO_async_tpool *
 PIO_Util::PIO_async_tpool_manager::~PIO_async_tpool_manager()
 {
   if(tpool_){
+    LOG((2, "PIO_async_tpool_manager:~PIO_async_tpool_manager: Finalizing tpool"));
     tpool_->finalize();
     delete(tpool_);
     tpool_ = NULL;
+  }
+  else{
+    LOG((2, "PIO_async_tpool_manager:~PIO_async_tpool_manager: tpool is already finalized/deleted"));
   }
 }
 
@@ -100,6 +116,7 @@ int pio_async_tpool_create(void )
    * when to send signals to async threads to finish queue async ops
    * (Note: Multiple iosystems init/finalize but use the same thread pool)
    */
+  LOG((2, "pio_async_tpool_create: Creating tpool (by ref)"));
   pio_async_init_cnt++;
 
   return PIO_NOERR;
@@ -110,6 +127,7 @@ int pio_async_tpool_op_add(pio_async_op_t *op)
   int ret;
   PIO_Util::PIO_async_tpool *tpool = tpool_mgr.get_tpool_instance();
   assert(tpool);
+  LOG((2, "pio_async_tpool_op_add(): Adding op"));
   tpool->enqueue(op);
 
   return PIO_NOERR;
@@ -127,12 +145,16 @@ int pio_async_tpool_finalize(void )
 {
   pio_async_init_cnt--;
   if(pio_async_init_cnt == 0){
+    LOG((2, "pio_async_tpool_finalize: Finalizing tpool instance"));
     PIO_Util::PIO_async_tpool *tpool = tpool_mgr.get_tpool_instance();
     assert(tpool);
     /* Signal threads waiting on queue to finish up/complete
      * the queued async ops and exit
      */
     tpool->finalize();
+  }
+  else{
+    LOG((2, "pio_async_tpool_finalize: Decrement ref cnt"));
   }
 
   return PIO_NOERR;
