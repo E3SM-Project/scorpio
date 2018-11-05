297814447072c847d6e0c0ea0f8fe9286f4d304c
jayeshkrishna <jayesh@mcs.anl.gov>
2018-11-05 12:54:52 -0600

    Adding some debug log stmts
    Adding more info to some existing log statements and adding some
    new ones.
    
    No change in code functionality.

--- src/clib/pio_darray.c
+++ src/clib/pio_darray.c
@@ -131,9 +131,9 @@ int PIOc_write_darray_multi(int ncid, const int *varids, int ioid, int nvars,
         if (varids[v] < 0 || varids[v] > PIO_MAX_VARS)
             return pio_err(ios, file, PIO_EINVAL, __FILE__, __LINE__);
 
-    LOG((1, "PIOc_write_darray_multi ncid = %d ioid = %d nvars = %d arraylen = %ld "
+    LOG((1, "PIOc_write_darray_multi file = %s ncid = %d ioid = %d nvars = %d arraylen = %ld "
          "flushtodisk = %d",
-         ncid, ioid, nvars, arraylen, flushtodisk));
+         file->fname, ncid, ioid, nvars, arraylen, flushtodisk));
 
     /* Check that we can write to this file. */
     if (!(file->mode & PIO_WRITE))
