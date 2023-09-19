a8a00e8215a44d19b40e63273fa1a67d8dddcfec
dqwu <wuda@mcs.anl.gov>
2023-09-19 13:49:52 -0500

    Updating example code

--- examples/c/example1.c
+++ examples/c/example1.c
@@ -11,18 +11,17 @@ A simple "3 x 6" 2D array, with 1-based indices (offsets) shown as below:
  7   8   9  10  11  12
 13  14  15  16  17  18
 
-rank 0 writes 5 values at: {1, 2, 3, 4, 5}
-rank 1 writes 4 values at: {6, 7, 8, 9}
-rank 2 writes 5 values at: {10, 11, 12, 13, 14}
-rank 3 writes 4 values at: {15, 16, 17, 18}
+[Description of the decompostion map]
+rank 0 writes 5 values at: {11, 10, 14, 13, 12}
+rank 1 writes 4 values at: {17, 18, 15, 16}
+rank 2 writes 5 values at: {3, 1, 5, 2, 4}
+rank 3 writes 4 values at: {9, 8, 6, 7}
 
-For rank i which writes at index j, the value to be written is "100 * (i + 1) + j"
-101   102   103   104   105   206
-207   208   209   310   311   312
-313   314   415   416   417   418
-
-Decomposition A uses ordered indices (offests, dof) to write variable A
-Decomposition B uses non-ordered indices (offests, dof) to write variable B
+For rank i which writes at offset j, define the value to be written as "100 * (i + 1) + j".
+This can make verification easier.
+301   302   303   304   305   406
+407   408   409   110   111   112
+113   114   215   216   217   218
 
 Expected ncdump:
 
@@ -31,19 +30,13 @@ dimensions:
 	row = 3 ;
 	col = 6 ;
 variables:
-	int VarA(row, col) ;
-	int VarB(row, col) ;
+	int var(row, col) ;
 data:
 
- VarA =
-  101, 102, 103, 104, 105, 206,
-  207, 208, 209, 310, 311, 312,
-  313, 314, 415, 416, 417, 418 ;
-
- VarB =
-  101, 102, 103, 104, 105, 206,
-  207, 208, 209, 310, 311, 312,
-  313, 314, 415, 416, 417, 418 ;
+ var =
+  301, 302, 303, 304, 305, 406,
+  407, 408, 409, 110, 111, 112,
+  113, 114, 215, 216, 217, 218 ;
 }
 */
   
@@ -60,14 +53,11 @@ int main(int argc, char* argv[])
     int dim_lens[2] = {3, 6};
     int iosysid;
     int ncid;
-    int varidA;
-    int varidB;
-    int ioidA;
-    int ioidB;
-    int bufferA[5];
-    int bufferB[5];
-    PIO_Offset compdofA[5];
-    PIO_Offset compdofB[5];
+    int varid;
+    int ioid;
+    int write_buffer[5];
+    int read_buffer[5];
+    PIO_Offset compdof[5];
 
 #ifdef TIMING    
   GPTLinitialize();
@@ -83,56 +73,64 @@ int main(int argc, char* argv[])
     niotasks = ntasks; 
     PIOc_Init_Intracomm(MPI_COMM_WORLD, niotasks, ioproc_stride, ioproc_start, PIO_REARR_BOX, &iosysid);
 
-    /* Decompostion A uses ordered indices (offests, dof), decomposition B uses shuffled indices */
     if (my_rank == 0)
     {
         elements_per_pe = 5;
-        compdofA[0] = 1; compdofA[1] = 2; compdofA[2] = 3; compdofA[3] = 4; compdofA[4] = 5;
-        compdofB[0] = 3; compdofB[1] = 1; compdofB[2] = 5; compdofB[3] = 2; compdofB[4] = 4;
+        compdof[0] = 11; compdof[1] = 10; compdof[2] = 14; compdof[3] = 13; compdof[4] = 12;
     }
     else if (my_rank == 1)
     {
         elements_per_pe = 4;
-        compdofA[0] = 6; compdofA[1] = 7; compdofA[2] = 8; compdofA[3] = 9;
-        compdofB[0] = 9; compdofB[1] = 8; compdofB[2] = 6; compdofB[3] = 7;
+        compdof[0] = 17; compdof[1] = 18; compdof[2] = 15; compdof[3] = 16;
     }
     else if (my_rank == 2)
     {
         elements_per_pe = 5;
-        compdofA[0] = 10; compdofA[1] = 11; compdofA[2] = 12; compdofA[3] = 13; compdofA[4] = 14;
-        compdofB[0] = 11; compdofB[1] = 10; compdofB[2] = 14; compdofB[3] = 13; compdofB[4] = 12;
+        compdof[0] = 3; compdof[1] = 1; compdof[2] = 5; compdof[3] = 2; compdof[4] = 4;
     }
     else if (my_rank == 3)
     {
         elements_per_pe = 4;
-        compdofA[0] = 15; compdofA[1] = 16; compdofA[2] = 17; compdofA[3] = 18;
-        compdofB[0] = 17; compdofB[1] = 18; compdofB[2] = 15; compdofB[3] = 16;
+        compdof[0] = 9; compdof[1] = 8; compdof[2] = 6; compdof[3] = 7;
     }
 
-    /* Set write buffers: for rank i which writes at index j, the value to be written is "100 * (i + 1) + j" */
+    /* Set write buffer: for rank i which writes at index j, we know the value to be written is "100 * (i + 1) + j" */
+    /* Also initialize read buffer */
     for (int i = 0; i < elements_per_pe; i++)
     {
-        bufferA[i] = 100 * (my_rank + 1) + compdofA[i];
-        bufferB[i] = 100 * (my_rank + 1) + compdofB[i];
+         write_buffer[i] = 100 * (my_rank + 1) + compdof[i];
+         read_buffer[i] = -1;
     }
 
-    PIOc_InitDecomp(iosysid, PIO_INT, 2, dim_lens, elements_per_pe, compdofA, &ioidA, NULL, NULL, NULL);
-    PIOc_InitDecomp(iosysid, PIO_INT, 2, dim_lens, elements_per_pe, compdofB, &ioidB, NULL, NULL, NULL);
+    PIOc_InitDecomp(iosysid, PIO_INT, 2, dim_lens, elements_per_pe, compdof, &ioid, NULL, NULL, NULL);
 
     PIOc_createfile(iosysid, &ncid, &format, "test_decomp.nc", PIO_CLOBBER);
     PIOc_def_dim(ncid, "row", (PIO_Offset)dim_lens[0], &dimids[0]);
     PIOc_def_dim(ncid, "col", (PIO_Offset)dim_lens[1], &dimids[1]);
-    PIOc_def_var(ncid, "VarA", PIO_INT, 2, dimids, &varidA);
-    PIOc_def_var(ncid, "VarB", PIO_INT, 2, dimids, &varidB);
+    PIOc_def_var(ncid, "var", PIO_INT, 2, dimids, &varid);
     PIOc_enddef(ncid);
 
-    PIOc_write_darray(ncid, varidA, ioidA, elements_per_pe, bufferA, NULL);
-    PIOc_write_darray(ncid, varidB, ioidB, elements_per_pe, bufferB, NULL);
+    PIOc_write_darray(ncid, varid, ioid, elements_per_pe, write_buffer, NULL);
+
+    PIOc_closefile(ncid);
+
+    PIOc_openfile(iosysid, &ncid, &format, "test_decomp.nc", PIO_NOWRITE);
+
+    PIOc_inq_varid(ncid, "var", &varid);
+    PIOc_read_darray(ncid, varid, ioid, elements_per_pe, read_buffer);
+
+    for (int i = 0; i < elements_per_pe; i++)
+    {
+        if (read_buffer[i] != write_buffer[i])
+        {
+            printf("rank = %d, read wrong data at index %d\n", my_rank, i);
+            return -1;
+        }
+    }
 
     PIOc_closefile(ncid);
 	
-    PIOc_freedecomp(iosysid, ioidA);
-    PIOc_freedecomp(iosysid, ioidB);
+    PIOc_freedecomp(iosysid, ioid);
 
     PIOc_finalize(iosysid);
 
