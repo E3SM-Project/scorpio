2e5cc9ed72a7f02862a7c5c52ad1eae0e46d7be2
jayeshkrishna <jayesh@mcs.anl.gov>
2024-09-19 07:13:09 -0500

    Adding driver and headers for replay tool
    Adding a template driver and source headers (I/O system specific
    and driver) for the replay tool.
    
    Also refactoring the log to source transformation class to a
    generic transformer class and sub classes to handle driver and
    I/O system source transformations

---
+++ tools/replay/src_templates/driver_template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include "mpi.h"
+
+#include "spio_replay_driver.hpp"
+
+int main(int argc, char *argv[])
+{
+  int rank = -1, sz = 0;
+  int ret = MPI_SUCCESS;
+  std::string banner_line = "=========================================";
+  std::string tool_banner = banner_line + "\n" +
+                            "\tSCORPIO REPLAY TOOL\n" +
+                            banner_line;
+
+  ret = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  assert(ret == MPI_SUCCESS);
+
+  ret = MPI_Comm_size(MPI_COMM_WORLD, &sz);
+  assert(ret == MPI_SUCCESS);
+
+  if(rank == 0){
+    std::cout << tool_banner.c_str() << "\n";
+  }
+
+  __DRIVER_RUN_SEQUENCE__
+
+  if(rank == 0){
+    std::cout << banner_line.c_str() << "\n";
+  }
+}
+
