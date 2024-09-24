a9d94b800bebf817f02aa767fd5587b5d3d2ce14
jayeshkrishna <jayesh@mcs.anl.gov>
2024-09-23 21:47:10 -0500

    Adding run method and capture run sequence
    Adding the run methods for I/O systems, capture the run sequence
    and use it to update the driver, and init/run/finalize the I/O
    systems based on the captured run sequence

--- tools/replay/src_templates/driver_template.cpp
+++ tools/replay/src_templates/driver_template.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <functional>
 #include <string>
 #include <cassert>
 #include "mpi.h"
@@ -27,7 +29,31 @@ int main(int argc, char *argv[])
     std::cout << tool_banner.c_str() << "\n";
   }
 
-  __DRIVER_RUN_SEQUENCE__
+  const std::vector<spio_replay_driver::iosys> iosys_infos = {__DRIVER_IOSYS_INFO__};
+  for(std::size_t i = 0; i < iosys_infos.size(); i++){
+    int ret = iosys_infos[i].init();
+    if(ret != 0){
+      std::cerr << "ERROR: Error initializing I/O system : " << i
+        << "(" << iosys_infos[i].info.c_str() << ")\n";
+    }
+  }
+
+  const std::vector<int> iosys_run_idx = {__DRIVER_RUN_SEQUENCE__};
+  for(std::size_t i = 0; i < iosys_run_idx.size(); i++){
+    int ret = iosys_infos[i].run();
+    if(ret != 0){
+      std::cerr << "ERROR: Error running I/O system : " << i
+        << "(" << iosys_infos[i].info.c_str() << ")\n";
+    }
+  }
+
+  for(std::size_t i = 0; i < iosys_infos.size(); i++){
+    int ret = iosys_infos[i].finalize();
+    if(ret != 0){
+      std::cerr << "ERROR: Error finalizing I/O system : " << i
+        << "(" << iosys_infos[i].info.c_str() << ")\n";
+    }
+  }
 
   if(rank == 0){
     std::cout << banner_line.c_str() << "\n";
@@ -35,5 +61,6 @@ int main(int argc, char *argv[])
 
   ret = MPI_Finalize();
   assert(ret == MPI_SUCCESS);
-}
 
+  return 0;
+}
