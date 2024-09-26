c2a9319d84c6e6f235afc15aadf3f9d07c3329ff
Jayesh Krishna <jayesh@mcs.anl.gov>
2024-09-26 11:15:54 -0500

    Handle MPI types differently in tracing
    Explicitly handle tracing of MPI types (MPI_Datatype, MPI_Comm).
    Since its hard (without explicitly mapping each type to a
    string) to convert opaque MPI types in a portable way we just
    add placeholders for now
    
    This change adds support for tracing with the OpenMPI library.

--- src/clib/spio_tracer.hpp
+++ src/clib/spio_tracer.hpp
@@ -39,6 +39,12 @@ namespace SPIO_Util{
         template<typename T>
         Timed_func_call_tracer &add_arg(const std::string &arg_name, const T *arg, std::size_t arg_sz);
 
+        /* Set the MPI args of the function that need to be traced - MPI opaque consts need special treatment */
+        template<typename T>
+        Timed_func_call_tracer &add_mpi_arg(const std::string &arg_name, const T &arg);
+        template<typename T>
+        Timed_func_call_tracer &add_mpi_arg(const std::string &arg_name, const T *arg, std::size_t arg_sz);
+
         /* Add the return values that need to be traced */
         template<typename T>
         Timed_func_call_tracer &add_rval(const std::string &name, const T &rval);
@@ -57,6 +63,9 @@ namespace SPIO_Util{
 
         template<typename T>
         static std::string arr_to_string(const T *arr, std::size_t arr_sz);
+
+        template<typename T>
+        static std::string mpi_arr_to_string(const T *arr, std::size_t arr_sz);
       private:
         /* The name of the function being traced */
         int func_id_;
@@ -108,9 +117,20 @@ namespace SPIO_Util{
     SPIO_Util::Logger::MPI_logger<std::ofstream> &get_iosys_trace_logger(int iosysid, int mpi_wrank);
     SPIO_Util::Logger::MPI_logger<std::ofstream> &get_file_trace_logger(int fh, int mpi_rank);
     void finalize_iosys_trace_logger(std::string iosys_key);
+
+    template<typename T>
+    std::string mpi_type_to_string(const T& t);
     //void finalize_file_trace_logger(int fh);
   } // namespace Tracer
 } // namespace SPIO_Util
+
+template<typename T>
+std::string SPIO_Util::Tracer::mpi_type_to_string(const T &t)
+{
+  /* FIXME: No easy/portable way to convert MPI_Datatype to string. Add mapping for all datatypes and use it later */
+  static int i = 0;
+  return std::string("MPI_Opaque_Datatype_") + std::to_string(i++);
+}
 
 template<typename T>
 SPIO_Util::Tracer::Timed_func_call_tracer &SPIO_Util::Tracer::Timed_func_call_tracer::add_arg(const std::string &arg_name, const T &arg)
@@ -138,6 +158,21 @@ template<typename T>
 SPIO_Util::Tracer::Timed_func_call_tracer &SPIO_Util::Tracer::Timed_func_call_tracer::add_arg(const std::string &arg_name, const T *arg, std::size_t arg_sz)
 {
   std::string arr_arg = arr_to_string(arg, arg_sz);
+  args_.push_back(std::pair<std::string, std::string>(arg_name, arr_arg));
+  return *this;
+}
+
+template<typename T>
+SPIO_Util::Tracer::Timed_func_call_tracer &SPIO_Util::Tracer::Timed_func_call_tracer::add_mpi_arg(const std::string &arg_name, const T &arg)
+{
+  args_.push_back(std::pair<std::string, std::string>(arg_name, SPIO_Util::Tracer::mpi_type_to_string(arg)));
+  return *this;
+}
+
+template<typename T>
+SPIO_Util::Tracer::Timed_func_call_tracer &SPIO_Util::Tracer::Timed_func_call_tracer::add_mpi_arg(const std::string &arg_name, const T *arg, std::size_t arg_sz)
+{
+  std::string arr_arg = mpi_arr_to_string(arg, arg_sz);
   args_.push_back(std::pair<std::string, std::string>(arg_name, arr_arg));
   return *this;
 }
@@ -182,6 +217,23 @@ std::string SPIO_Util::Tracer::Timed_func_call_tracer::arr_to_string(const T *ar
     for(std::size_t i = 1; i < arr_sz; i++){
       arr_str += ARG_SEP;
       arr_str += std::to_string(arr[i]);
+    }
+  }
+
+  arr_str += ARRAY_ARG_SUFFIX;
+  return arr_str;
+}
+
+template<typename T>
+std::string SPIO_Util::Tracer::Timed_func_call_tracer::mpi_arr_to_string(const T *arr, std::size_t arr_sz)
+{
+  std::string arr_str(1, ARRAY_ARG_PREFIX);
+
+  if(arr_sz > 0){
+    arr_str += SPIO_Util::Tracer::mpi_type_to_string(arr[0]);
+    for(std::size_t i = 1; i < arr_sz; i++){
+      arr_str += ARG_SEP;
+      arr_str += SPIO_Util::Tracer::mpi_type_to_string(arr[i]);
     }
   }
 
