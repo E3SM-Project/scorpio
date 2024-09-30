44be0129896d88a5caf1bbaaec6a0a3853bf6f1c
jayeshkrishna <jayesh@mcs.anl.gov>
2024-09-29 19:19:26 -0500

    Adding a MT Queue and thread pool
    Adding a multithreaded queue and a thread pool. Also adding util
    functions to use the new thread pool for asynchronous tasks.
    These new capabilities are not used by the library right now
    (although some interfaces have been modified/added to support
    usage in future)
    
    Adding a thread pool for asynchronously processing async_op tasks.
     * Allows queuing async tasks of pio_async_op_t type
     * Leverages the Multi-threaded queue, PIO_mtq, to keep track
       of async ops
     * The instances of the thread pool is managed via a thread pool
       manager.
     * Currently the number of async threads is set to 1
     * Also adding a simple unit test for the thread pool.
    
    Adding a multi-threaded queue that will be eventually used for
    queueing asynchronous I/O tasks.
     * Using C++11 threading framework and synchronization primitives
       to implement the multi-threaded queue.
       - Multiple threads can access the queue without any explicit
         synchronization (all required synchronization is handled by
         the queue)
       - Functions to enqueue/dequeue data in queue
       - Function to signal threads waiting on dequeuing data from
         the queue is also available (SIG_STOP : stop waiting on queue,
         SIG_COMPLETE : complete dequeing existing items and exit when
         when the queue is empty)
       - Function to provide size (instantaneous) of the queue
    
    Adding configure option PIO_USE_ASYNC_WR_THREAD to enable/disable
    asynchronous write threads
    
    Implementing the necessary framework to add asynchronous
    operations to a file or I/O system.
    
    Also adding unit tests for multithreaded queue & thread pool

---
+++ src/clib/spio_async_mtq.hpp
@@ -0,0 +1,167 @@
+#ifndef _SPIO_ASYNC_MTQ_HPP_
+#define _SPIO_ASYNC_MTQ_HPP_
+
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <chrono>
+#include <mutex>
+#include <condition_variable>
+extern "C"{
+#include "pio_internal.h"
+} // extern "C"
+
+namespace PIO_Util{
+
+template<typename T>
+class PIO_mtq{
+  public:
+    typedef enum SigTypes{
+      PIO_MTQ_SIG_INVALID=1,
+      /* Stop processing elements in the queue */
+      PIO_MTQ_SIG_STOP,
+      /* Complete processing elements in queue and quit */
+      PIO_MTQ_SIG_COMPLETE
+    }SigTypes_t;
+    PIO_mtq();
+    void enqueue(const T& val);
+    int dequeue(T &val);
+    void signal(SigTypes_t sig);
+    int size(void ) const;
+
+    template<typename U>
+    friend std::ostream & operator<<(std::ostream &ostr, PIO_mtq<U> &mtq);
+  private:
+    void thread_yield(std::unique_lock<std::mutex> &lk) const;
+    std::mutex mtx_;
+    std::queue<T> queue_;
+    std::condition_variable cv_;
+    SigTypes_t sig_;
+    std::condition_variable cv_sig_;
+};
+
+template<typename T>
+PIO_mtq<T>::PIO_mtq():sig_(PIO_mtq<T>::PIO_MTQ_SIG_INVALID)
+{
+  LOG((2, "PIO_mtq:PIO_mtq: Creating MT queue"));
+}
+
+template<typename T>
+void PIO_mtq<T>::enqueue(const T& val)
+{
+  std::unique_lock<std::mutex> lk(mtx_);
+  LOG((2, "PIO_mtq:enqueue: Enqueing val to mtq"));
+  queue_.push(val);
+  lk.unlock();
+  cv_.notify_all();
+}
+
+template<typename T>
+int PIO_mtq<T>::dequeue(T &val)
+{
+  std::unique_lock<std::mutex> lk(mtx_);
+  do{
+    LOG((2, "PIO_mtq:dequeue: Waiting for dequeueing val from mtq..."));
+    while(queue_.empty() && (sig_ == PIO_MTQ_SIG_INVALID)){
+      cv_.wait(lk, [this]{ return (queue_.empty() && (sig_ == PIO_MTQ_SIG_INVALID));}); 
+      /*
+      cv_.wait_for(lk, DEFAULT_TIMEOUT,
+              [this]{ return (queue_.empty() && (sig_ == PIO_MTQ_SIG_INVALID));}); 
+      */
+      /* At least on Linux (Ubuntu 4.5.0-040500-generic) just waiting on the condition
+       * variable (with/without timeouts) does not allow other threads to acquire
+       * the lock. So we explicitly unlock, sleep for 0 seconds and reacquire the 
+       * lock to allow other threads to be able to get scheduled and acquire the lock
+       */
+      thread_yield(lk);
+    }
+    if(sig_ == PIO_MTQ_SIG_STOP){
+      LOG((2, "PIO_mtq:dequeue: Received STOP signal on mtq (exiting...)"));
+      return 1;
+    }
+    else if(sig_ == PIO_MTQ_SIG_COMPLETE){
+      if(queue_.empty()){
+        /* Reset signal */
+        sig_ = PIO_MTQ_SIG_INVALID;
+        lk.unlock();
+        /* Notify threads that all async ops are complete */
+        cv_sig_.notify_all();
+        lk.lock();
+      }
+      else{
+        break;
+      }
+    }
+    else{
+      break;
+    }
+  }while(true);
+  val = queue_.front();
+  queue_.pop();
+  LOG((2, "PIO_mtq:dequeue: Successfully dequeued val from mtq"));
+  lk.unlock();
+  return 0;
+}
+
+template<typename T>
+void PIO_mtq<T>::signal(PIO_mtq<T>::SigTypes_t sig)
+{
+  std::unique_lock<std::mutex> lk(mtx_);
+  sig_ = sig;
+  lk.unlock();
+  cv_.notify_all();
+
+  lk.lock();
+  if(sig == PIO_MTQ_SIG_COMPLETE){
+    LOG((2, "PIO_mtq:signal: Received PIO_MTQ_SIG_COMPLETE, Waiting for async ops to complete"));
+    /* Wait for all async ops in the queue to complete */
+    while(sig_ == PIO_MTQ_SIG_COMPLETE){
+      cv_sig_.wait(lk, [this]{ return (sig_ == PIO_MTQ_SIG_COMPLETE);}); 
+      thread_yield(lk);
+    }
+  }
+  lk.unlock();
+}
+
+template<typename T>
+int PIO_mtq<T>::size(void ) const
+{
+  int sz = 0;
+  std::unique_lock<std::mutex> lk(mtx_);
+  sz = queue_.size();
+  lk.unlock();
+  return sz;
+}
+
+template<typename T>
+void PIO_mtq<T>::thread_yield(std::unique_lock<std::mutex> &lk) const
+{
+  /* At least on Linux (Ubuntu 4.5.0-040500-generic) just waiting on the condition
+   * variable (with/without timeouts) does not allow other threads to acquire
+   * the lock. So we explicitly unlock, sleep for 0 seconds and reacquire the 
+   * lock to allow other threads to be able to get scheduled and acquire the lock
+   */
+  const std::chrono::milliseconds ZERO_TIMEOUT = std::chrono::milliseconds(0);
+  lk.unlock();
+  //LOG((2, "PIO_mtq:thread_yield: Yielding for 0 secs"));
+  std::this_thread::sleep_for(ZERO_TIMEOUT);
+  lk.lock();
+}
+
+template<typename T>
+std::ostream &operator<<(std::ostream &ostr, PIO_Util::PIO_mtq<T> &q)
+{
+  std::unique_lock<std::mutex> lk(q.mtx_);
+  /* We need to copy the queue since it is not iterable */
+  std::queue<T> tq = q.queue_;
+  while(!tq.empty()){
+    ostr << tq.front() << ", ";
+    tq.pop();
+  }
+  lk.unlock();
+  return ostr;
+}
+
+} // namespace PIO_Util
+
+#endif // _SPIO_ASYNC_MTQ_HPP_
