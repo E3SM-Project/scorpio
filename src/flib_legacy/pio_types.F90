1dab253b8ff97d0df3ce6312e48bc93e4486fbdc
jayeshkrishna <jayesh@mcs.anl.gov>
2024-11-07 09:33:03 -0600

    Adding PIO_IOTYPE_ADIOSC to legacy fort interface
    Adding the new I/O type, PIO_IOTYPE_ADIOSC, that supports ADIOS
    output with data compression in the legacy Fortran interface

--- src/flib_legacy/pio_types.F90
+++ src/flib_legacy/pio_types.F90
@@ -101,6 +101,7 @@ module pio_types
 !!   - PIO_iotype_netcdf4c : parallel read/serial write of NetCDF4 (HDF5) files with data compression
 !!   - PIO_iotype_netcdf4p : parallel read/write of NETCDF4 (HDF5) files
 !!   - PIO_iotype_adios : parallel write of ADIOS files with subset rearrangement only
+!!   - PIO_iotype_adiosc : parallel write of ADIOS files with data compression
 !!   - PIO_iotype_hdf5 : parallel write of HDF5 files
 !>
     integer(i4), public, parameter ::  &
@@ -109,7 +110,8 @@ module pio_types
         PIO_iotype_netcdf4c = 3, &  ! netcdf4 (hdf5 format) file opened for compression (serial write access only)
         PIO_iotype_netcdf4p = 4, &  ! netcdf4 (hdf5 format) file opened in parallel (all netcdf4 files for read will be opened this way)
         PIO_iotype_adios = 5, &     ! parallel write of ADIOS files (Write only, rearr subset only)
-        PIO_iotype_hdf5 = 6         ! parallel write of HDF5 files
+        PIO_iotype_adiosc = 6, &    ! parallel write of ADIOS files with data compression
+        PIO_iotype_hdf5 = 7         ! parallel write of HDF5 files
 
 
 ! These are for backward compatability and should not be used or expanded upon
