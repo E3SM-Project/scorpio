eede683d3901f17849fccb9587c968a21559fdb5
jayeshkrishna <jayesh@mcs.anl.gov>
2025-01-15 08:37:51 -0600

    Adding some simple PnetCDF tests
    Adding some PnetCDF tests to write 1d, 2d, 3d vars

---
+++ tests/pnetcdf/test_pnetcdf.c
@@ -0,0 +1,272 @@
+/*********************************************************************
+ *
+ *  Copyright (C) 2013, Northwestern University and Argonne National Laboratory
+ *  See COPYRIGHT notice in top-level directory.
+ *
+ *********************************************************************/
+/* $Id$ */
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * This example shows how to use ncmpi_put_vara_int_all() to write a 2D 4-byte
+ * integer array in parallel. It first defines a netCDF variable of size
+ * global_ny * global_nx where
+ *    global_ny == NY and
+ *    global_nx == (NX * number of MPI processes).
+ * The data partitioning pattern is a column-wise partitioning across all
+ * processes. Each process writes a subarray of size ny * nx.
+ *
+ *    To compile:
+ *        mpicc -O2 put_vara.c -o put_vara -lpnetcdf
+ *
+ * Example commands for MPI run and outputs from running ncmpidump on the
+ * output netCDF file produced by this example program:
+ *
+ *    % mpiexec -n 4 ./put_vara /pvfs2/wkliao/testfile.nc
+ *
+ *    % ncmpidump /pvfs2/wkliao/testfile.nc
+ *    netcdf testfile {
+ *    // file format: CDF-5 (big variables)
+ *    dimensions:
+ *            y = 10 ;
+ *            x = 16 ;
+ *    variables:
+ *            int var(y, x) ;
+ *                var:str_att_name = "example attribute of type text." ;
+ *                var:float_att_name = 0.f, 1.f, 2.f, 3.f, 4.f, 5.f, 6.f, 7.f ;
+ *                var:short_att_name = 1000s ;
+ *    // global attributes:
+ *                :history = "Mon Aug 13 21:27:48 2018" ;
+ *       "" ;
+ *    data:
+ *
+ *     var =
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3,
+ *         0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3 ;
+ *    }
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> /* strcpy(), strncpy() */
+#include <unistd.h> /* getopt() */
+#include <time.h>   /* time() localtime(), asctime() */
+#include <mpi.h>
+#include <pnetcdf.h>
+
+#define NZ 2
+#define NY 10
+#define NX 4
+
+static int verbose;
+
+#define ERR {if(err!=NC_NOERR){printf("Error at %s:%d : %s\n", __FILE__,__LINE__, ncmpi_strerror(err));nerrs++;}}
+
+static void
+usage(char *argv0)
+{
+    char *help =
+    "Usage: %s [-h] | [-q] [-k format] [file_name]\n"
+    "       [-h] Print help\n"
+    "       [-q] Quiet mode (reports when fail)\n"
+    "       [-k format] file format: 1 for CDF-1, 2 for CDF-2, 3 for NetCDF4,\n"
+    "                                4 for NetCDF4 classic model, 5 for CDF-5\n"
+    "       [filename] output netCDF file name\n";
+    fprintf(stderr, help, argv0);
+}
+
+/*----< pnetcdf_check_mem_usage() >------------------------------------------*/
+/* check PnetCDF library internal memory usage */
+static int
+pnetcdf_check_mem_usage(MPI_Comm comm)
+{
+    int err, nerrs=0, rank;
+    MPI_Offset malloc_size, sum_size;
+
+    MPI_Comm_rank(comm, &rank);
+
+    /* print info about PnetCDF internal malloc usage */
+    err = ncmpi_inq_malloc_max_size(&malloc_size);
+    if (err == NC_NOERR) {
+        MPI_Reduce(&malloc_size, &sum_size, 1, MPI_OFFSET, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0 && verbose)
+            printf("maximum heap memory allocated by PnetCDF internally is %lld bytes\n",
+                   sum_size);
+
+        /* check if there is any PnetCDF internal malloc residue */
+        err = ncmpi_inq_malloc_size(&malloc_size);
+        MPI_Reduce(&malloc_size, &sum_size, 1, MPI_OFFSET, MPI_SUM, 0, MPI_COMM_WORLD);
+        if (rank == 0 && sum_size > 0)
+            printf("heap memory allocated by PnetCDF internally has %lld bytes yet to be freed\n",
+                   sum_size);
+    }
+    else if (err != NC_ENOTENABLED) {
+        printf("Error at %s:%d: %s\n", __FILE__,__LINE__,ncmpi_strerror(err));
+        nerrs++;
+    }
+    return nerrs;
+}
+
+/*----< pnetcdf_io() >-------------------------------------------------------*/
+static int
+pnetcdf_io(MPI_Comm comm, char *filename, int cmode)
+{
+    int i, j, z, rank, nprocs, err, nerrs=0;
+    int ncid, var1did, var2did, var3did, dimid[3], buf1d[NX], buf2d[NY][NX], buf3d[NZ][NY][NX];
+    char str_att[128];
+    float float_att[100];
+    MPI_Offset  global_nz, global_ny, global_nx;
+    MPI_Offset start[3], count[3];
+
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &nprocs);
+
+    /* create a new file for writing ----------------------------------------*/
+    cmode |= NC_CLOBBER;
+    err = ncmpi_create(comm, filename, cmode, MPI_INFO_NULL, &ncid); ERR
+
+    for (i=0; i<NX; i++)
+         buf1d[i] = rank;
+
+    /* the global array is NY * (NX * nprocs) */
+    global_nz = NZ;
+    global_ny = NY;
+    global_nx = NX * nprocs;
+
+    for (i=0; i<NY; i++)
+        for (j=0; j<NX; j++)
+             buf2d[i][j] = rank;
+
+    for (z=0; z<NZ; z++)
+        for (i=0; i<NY; i++)
+            for (j=0; j<NX; j++)
+                   buf3d[z][i][j] = rank + z * nprocs;
+
+    /* add a global attribute: a time stamp at rank 0 */
+    time_t ltime = time(NULL); /* get the current calendar time */
+    asctime_r(localtime(&ltime), str_att);
+    sprintf(str_att, "Mon Aug 13 21:27:48 2018");
+
+    /* make sure the time string are consistent among all processes */
+    MPI_Bcast(str_att, sizeof(str_att), MPI_CHAR, 0, comm);
+
+    err = ncmpi_put_att_text(ncid, NC_GLOBAL, "history", strlen(str_att),
+                             &str_att[0]); ERR
+
+    /* define dimensions x and y */
+    err = ncmpi_def_dim(ncid, "Z", global_nz, &dimid[0]); ERR
+    err = ncmpi_def_dim(ncid, "Y", global_ny, &dimid[1]); ERR
+    err = ncmpi_def_dim(ncid, "X", global_nx, &dimid[2]); ERR
+
+    /* define a 1D variable of integer type */
+    err = ncmpi_def_var(ncid, "var1d", NC_INT, 1, &dimid[2], &var1did); ERR
+
+    /* define a 2D variable of integer type */
+    err = ncmpi_def_var(ncid, "var2d", NC_INT, 2, &dimid[1], &var2did); ERR
+
+    /* define a 2D variable of integer type */
+    err = ncmpi_def_var(ncid, "var3d", NC_INT, 3, &dimid[0], &var3did); ERR
+
+    /* add attributes to the variable */
+    strcpy(str_att, "example attribute of type text.");
+    err = ncmpi_put_att_text(ncid, var2did, "str_att_name", strlen(str_att),
+                             &str_att[0]); ERR
+
+    for (i=0; i<8; i++) float_att[i] = i;
+    err = ncmpi_put_att_float(ncid, var2did, "float_att_name", NC_FLOAT, 8,
+                              &float_att[0]); ERR
+    short short_att=1000;
+    err = ncmpi_put_att_short(ncid, var2did, "short_att_name", NC_SHORT, 1,
+                              &short_att); ERR
+
+    /* do not forget to exit define mode */
+    err = ncmpi_enddef(ncid); ERR
+
+    /* now we are in data mode */
+    start[0] = NX * rank;
+    count[0] = NX;
+
+    err = ncmpi_put_vara_int_all(ncid, var1did, start, count, &buf1d[0]); ERR
+
+    start[0] = 0;
+    start[1] = NX * rank;
+    count[0] = NY;
+    count[1] = NX;
+
+    err = ncmpi_put_vara_int_all(ncid, var2did, start, count, &buf2d[0][0]); ERR
+
+    start[0] = 0;
+    start[1] = 0;
+    start[2] = NX * rank;
+    count[0] = NZ;
+    count[1] = NY;
+    count[2] = NX;
+
+    err = ncmpi_put_vara_int_all(ncid, var3did, start, count, &buf3d[0][0][0]); ERR
+
+    err = ncmpi_close(ncid); ERR
+
+    return nerrs;
+}
+
+int main(int argc, char** argv)
+{
+    extern int optind;
+    extern char *optarg;
+    char filename[256];
+    int i, rank, kind=0, cmode=0, nerrs=0;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    verbose = 1;
+
+    /* get command-line arguments */
+    while ((i = getopt(argc, argv, "hqk:")) != EOF)
+        switch(i) {
+            case 'q': verbose = 0;
+                      break;
+            case 'k': kind = atoi(optarg);
+                      break;
+            case 'h':
+            default:  if (rank==0) usage(argv[0]);
+                      MPI_Finalize();
+                      return 1;
+        }
+    if (argv[optind] == NULL) strcpy(filename, "testfile.nc");
+    else                      snprintf(filename, 256, "%s", argv[optind]);
+
+    MPI_Bcast(filename, 256, MPI_CHAR, 0, MPI_COMM_WORLD);
+
+    if (verbose && rank == 0) printf("%s: example of using put_vara APIs\n",__FILE__);
+
+    switch (kind) {
+        case(2): cmode = NC_64BIT_OFFSET;             break;
+        case(3): cmode = NC_NETCDF4;                  break;
+        case(4): cmode = NC_NETCDF4|NC_CLASSIC_MODEL; break;
+        case(5): cmode = NC_64BIT_DATA;               break;
+        default: cmode = 0;
+    }
+
+#ifndef PNETCDF_DRIVER_NETCDF4
+    /* netcdf4 driver is not enabled, skip */
+    if (kind == 3 || kind == 4) {
+        MPI_Finalize();
+        return 0;
+    }
+#endif
+    nerrs += pnetcdf_io(MPI_COMM_WORLD, filename, cmode);
+
+    nerrs += pnetcdf_check_mem_usage(MPI_COMM_WORLD);
+
+    MPI_Finalize();
+    return (nerrs > 0);
+}
+
