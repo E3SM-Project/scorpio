c7e5abc136cc2acbe4655a363a46f74c254a36f2
jayeshkrishna <jayesh@mcs.anl.gov>
2025-01-25 17:11:21 -0600

    Add iosysid to decomp map info file name
    Adding iosysid to the meta data file containing decomposition
    map info (mapping btw decomp and file/vars). The file name is
    now pio_decomp_map_info_<IOSYSID>.txt

--- src/clib/spio_decomp_map_info_pool.cpp
+++ src/clib/spio_decomp_map_info_pool.cpp
@@ -25,12 +25,14 @@ SPIO_Util::Decomp_Util::Decomp_map_info_pool_manager::~Decomp_map_info_pool_mana
   }
 }
 
-void SPIO_Util::Decomp_Util::serialize_decomp_map_info_pool(MPI_Comm comm)
+void SPIO_Util::Decomp_Util::serialize_decomp_map_info_pool(iosystem_desc_t *ios)
 {
   static bool decomp_info_serialized = false;
 
+  assert(ios);
   if(decomp_info_serialized) return;
 
+  MPI_Comm comm = ios->union_comm;
   int rank = -1;
   MPI_Comm_rank(comm, &rank);
   if(rank != 0) return;
@@ -38,7 +40,8 @@ void SPIO_Util::Decomp_Util::serialize_decomp_map_info_pool(MPI_Comm comm)
   SPIO_Util::Decomp_Util::Decomp_map_info_pool *dpool = SPIO_Util::Decomp_Util::gdpool_mgr.get_decomp_map_info_pool();
   std::string decomp_map_pool_info = dpool->to_string();
   
-  std::ofstream ostr("pio_decomp_map_info.txt");
+  std::string fname = std::string("pio_decomp_map_info_") + std::to_string(ios->iosysid) + std::string(".txt");
+  std::ofstream ostr(fname.c_str());
   if(ostr.is_open()){
     ostr << decomp_map_pool_info.c_str() << "\n";
     ostr.close();
