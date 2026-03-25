2ae824769b8de73c5fd3a4ff9c6daaab2042a573
jayeshkrishna <jayesh@mcs.anl.gov>
2026-03-25 10:56:29 -0500

    Adding log level to mpi logger
    Adding a log level to the MPI logger.
    
    Also adding an option to explicitly enable logging on a process

--- util/spio_logger.hpp
+++ util/spio_logger.hpp
@@ -7,13 +7,23 @@
 
 namespace SPIO_Util{
   namespace Logger{
+    enum class Log_level : int{
+      INVALID = 0,
+      TRACE,
+      DEBUG,
+      INFO,
+      WARN,
+      ERROR,
+      FATAL
+    };
+
     /* FIXME: Add a generic logger class */
     /* FIXME : Allow logging from multiple classes */
     /* An MPI logger */
     template<typename TStream>
     class MPI_logger{
       public:
-        MPI_logger() : mpi_comm_(MPI_COMM_NULL), ostr_(NULL), is_io_proc_(false) {}
+        MPI_logger() : mpi_comm_(MPI_COMM_NULL), ostr_(NULL), is_io_proc_(false), slog_done_(false), log_lvl_(Log_level::INFO) {}
 
         MPI_logger(MPI_Comm comm, TStream *ostr);
 
@@ -21,11 +31,20 @@ namespace SPIO_Util{
 
         MPI_logger &operator=(const MPI_logger &other) = default;
 
+        /* Enable logging on this proc. By default logging is only enabled on rank 0 */
+        MPI_logger &enable_logging(void ) { is_io_proc_ = true;  return *this; }
+
         /* Log message */
         void log(const std::string &log_msg);
 
+        /* Log message, with log level */
+        void log(Log_level lvl, const std::string &log_msg);
+
         /* Singleton log : Logging is only done once - e.g. writing headers etc */
         void slog(const std::string &slog_msg);
+
+        /* Singleton log with log level : Logging is only done once - e.g. writing headers etc */
+        void slog(Log_level lvl, const std::string &slog_msg);
 
         /* Explicitly flush the contents of the logger */
         void flush(void );
@@ -39,22 +58,24 @@ namespace SPIO_Util{
         TStream *ostr_;
         bool is_io_proc_;
         bool slog_done_;
+        Log_level log_lvl_;
 
-        static const int MPI_RANK_ROOT = 0;
+        static const int MPI_DEFAULT_ROOT = 0;
         static const char LOG_SEP = '\n';
     };
   } // namespace Logger
 } // namespace SPIO_Util
 
 template<typename TStream>
-SPIO_Util::Logger::MPI_logger<TStream>::MPI_logger(MPI_Comm comm, TStream *ostr):mpi_comm_(comm), ostr_(ostr), is_io_proc_(false), slog_done_(false)
+SPIO_Util::Logger::MPI_logger<TStream>::MPI_logger(MPI_Comm comm, TStream *ostr):
+  mpi_comm_(comm), ostr_(ostr), is_io_proc_(false), slog_done_(false), log_lvl_(Log_level::INFO)
 {
   int rank;
   int ret;
 
   ret = MPI_Comm_rank(mpi_comm_, &rank);
   assert(ret == MPI_SUCCESS);
-  if(rank == MPI_RANK_ROOT){
+  if(rank == MPI_DEFAULT_ROOT){
     is_io_proc_ = true;
   }
 }
@@ -62,7 +83,13 @@ SPIO_Util::Logger::MPI_logger<TStream>::MPI_logger(MPI_Comm comm, TStream *ostr)
 template<typename TStream>
 void SPIO_Util::Logger::MPI_logger<TStream>::log(const std::string &log_msg)
 {
-  if(is_io_proc_){
+  log(Log_level::INFO, log_msg);
+}
+
+template<typename TStream>
+void SPIO_Util::Logger::MPI_logger<TStream>::log(Log_level lvl, const std::string &log_msg)
+{
+  if(is_io_proc_ && (log_lvl_ <= lvl)){
     (*ostr_) << log_msg.c_str() << LOG_SEP;
   }
 }
@@ -70,8 +97,16 @@ void SPIO_Util::Logger::MPI_logger<TStream>::log(const std::string &log_msg)
 template<typename TStream>
 void SPIO_Util::Logger::MPI_logger<TStream>::slog(const std::string &slog_msg)
 {
+  slog(Log_level::INFO, slog_msg);
+}
+
+template<typename TStream>
+void SPIO_Util::Logger::MPI_logger<TStream>::slog(Log_level lvl, const std::string &slog_msg)
+{
   if(!slog_done_){
-    log(slog_msg);
+    if(is_io_proc_ && (log_lvl_ <= lvl)){
+      log(slog_msg);
+    }
     slog_done_ = true;
   }
 }
